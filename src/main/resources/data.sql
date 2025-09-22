c38f710f2a6667f95b2d7e348e726c71ed353ca9
Santhosh Kommireddy <kommireddysanthosh.com>
2025-09-22 12:45:57 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- This command inserts a new row into the 'admin' table.
+-- The ID will be generated automatically.
+INSERT INTO admin (email_id, password) VALUES ('kommireddysanthosh@gmail.com', 'Santhosh@2003');
